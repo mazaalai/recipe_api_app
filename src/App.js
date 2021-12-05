6c02e61f1db2f33fea5d13c55f01242f578178e6
Taivan Enkhbayar <r0745664@student.thomasmore.be>
2021-12-05 19:58:06 +0100

    1. Styled everything 2. Used Content for showing search result. 3. localstorage saves Objects of result.

--- src/App.js
+++ src/App.js
@@ -1,18 +1,23 @@
 import React, {useEffect, useState} from "react";
-import Recipe from "./Recipe";
+import Recipe from "./components/Recipe";
+import {RecipeContext, RecipeProvider} from "./contexts/Context";
 import './App.css';
 
 const App = () => {
+
     const APP_ID = "a07598a7";
     const APP_KEY = "1df30a30400703ed493e29de20fa11a7";
-
     const [recipes, setRecipes] = useState([]);
     const [search, setSearch] = useState('');
-    const [query, setQuery] = useState('chicken');
+    const [query, setQuery] = useState('Chicken');
 
     useEffect(() => {
         getRecipes();
     }, [query]);
+
+    useEffect(() => {
+        localStorage.setItem("recipes", JSON.stringify(recipes));
+    }, [recipes]);
 
     const getRecipes = async () => {
         const response = await fetch(`https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`)
@@ -24,33 +29,35 @@ const App = () => {
     const updateSearch = e => {
         setSearch(e.target.value);
     }
-    const getSearch = e =>{
+    const getSearch = e => {
         e.preventDefault();
         setQuery(search);
         setSearch('');
     }
     return (
-        <div className="App">
-            <form onSubmit={getSearch} className="search-form">
-                <input className="search-bar" type="text" value={search} onChange={updateSearch}/>
-                <button className="search-button" type="submit">
-                    Search
-                </button>
-            </form>
-            <div className="recipes">
-                {recipes.map(recipes => (
-                    <Recipe
-                        key={recipes.recipe.label}
-                        title={recipes.recipe.label}
-                        calories={recipes.recipe.calories}
-                        image={recipes.recipe.image}
-                        ingredients={recipes.recipe.ingredients}
-                        directions= {recipes.recipe.url}
-                    />
-                ))}
-            </div>
+        <RecipeContext.Provider value={query}>
+            <div className="App">
+                <h1>Zoek je eten en krijg ingrediënten.</h1>
+                <form onSubmit={getSearch} className="search-form">
+                    <input className="search-bar" type="text" value={search} onChange={updateSearch}/>
+                    <button className="search-button" type="submit">
+                        Search
+                    </button>
+                </form>
+                <div className="recipes">
+                    {recipes.map(recipes => (
+                        <Recipe
+                            key={recipes.recipe.label}
+                            title={recipes.recipe.label}
+                            image={recipes.recipe.image}
+                            ingredients={recipes.recipe.ingredients}
+                            directions={recipes.recipe.url}
+                        />
+                    ))}
+                </div>
 
-        </div>
+            </div>
+        </RecipeContext.Provider>
     )
 }
 export default App;
--- src/components/Recipe.js
+++ src/components/Recipe.js
@@ -1,16 +1,20 @@
-import React from "react";
-import style from './recipe.module.css';
+import React , {useContext}from "react";
+import {RecipeContext} from "../contexts/Context";
+import style from '../recipe.module.css';
 
-const Recipe = ({title, calories, image,ingredients, directions}) =>{
+const Recipe = ({key, title, image,ingredients, directions}) =>{
+    const value = useContext(RecipeContext)
     return(
         <div className={style.recipe}>
+            <br/>
+            <p>search result for {value}</p>
             <h1>{title}</h1>
             <ol>
                 {ingredients.map(ingredients => (
                     <li>{ingredients.text}</li>
                 ))}
             </ol>
-            <p>{calories}</p>
+
             <img className={style.image} src={image} alt="Image"/>
             <br/>
             <form method="get" action={directions} target='_blank'>
