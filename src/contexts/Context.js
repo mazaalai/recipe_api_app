a9f93c83464ad909b9ae7cea0a4e715adf400a2b
Taivan Enkhbayar <r0745664@student.thomasmore.be>
2021-12-05 23:55:00 +0100

    1. Data is coming from context. 2. Localstorage stores data.

--- src/contexts/Context.js
+++ src/contexts/Context.js
@@ -61,7 +61,7 @@ export function AllRecipesProvider(){
                     Search
                 </button>
             </form>
-            <p>*Zoek geschiedenis {searchHistory}</p>
+            <p>*Laatst gezocht recept. {searchHistory}</p>
             <RecipeList recipes={recipes}/>
 
         </div>
